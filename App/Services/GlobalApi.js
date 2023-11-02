73bf974287ef070c33fa3ccf0a9e261358e2dea8
S.Sanjeevi <ssanjeevi1999@gmail.com>
2023-11-02 13:29:23 +0530

    comment added

--- App/Services/GlobalApi.js
+++ App/Services/GlobalApi.js
@@ -1,12 +1,12 @@
 import { create } from "apisauce";
 
-
+// apiKey=1033e4cd1d724b57b7752cc0811d455f
 const api = create({
     baseURL: 'https://newsapi.org/v2',
   })
 const apiKey = '?sources=bbc-news&apiKey=1033e4cd1d724b57b7752cc0811d455f';
 
-// used apisause
+// used apisauce
 const getTopHeadline = api.get('/top-headlines'+apiKey);
 const getByCategory = (category) => api.get('/everything?q='+category+'&apiKey=1033e4cd1d724b57b7752cc0811d455f')
 
